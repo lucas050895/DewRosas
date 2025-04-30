1213206e72728394a5280dea4d63c9acc6f5d5c6
Lucas Conde <lucasconde742@gmail.com>
2025-04-30 19:31:20 -0300

    Modificaciones en el footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,21 +1,28 @@
 <footer>
-    <section class="redes">
-        <h2>Nuestras redes</h2>
-        <p>¡Siguenos en nuestras redes y estaras al tanto de todo!</p>
+    <svg id="wave" style="transform:rotate(0deg); transition: 0.3s" viewBox="0 0 1440 280" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+                <stop stop-color="#ec8fe6" offset="100%"></stop>
+            </linearGradient>
+        </defs>
+        <path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)" d="M0,140L48,154C96,168,192,196,288,205.3C384,215,480,205,576,196C672,187,768,177,864,154C960,131,1056,93,1152,107.3C1248,121,1344,187,1440,219.3C1536,252,1632,252,1728,252C1824,252,1920,252,2016,210C2112,168,2208,84,2304,70C2400,56,2496,112,2592,154C2688,196,2784,224,2880,219.3C2976,215,3072,177,3168,144.7C3264,112,3360,84,3456,102.7C3552,121,3648,187,3744,210C3840,233,3936,215,4032,191.3C4128,168,4224,140,4320,126C4416,112,4512,112,4608,102.7C4704,93,4800,75,4896,60.7C4992,47,5088,37,5184,46.7C5280,56,5376,84,5472,102.7C5568,121,5664,131,5760,130.7C5856,131,5952,121,6048,107.3C6144,93,6240,75,6336,70C6432,65,6528,75,6624,79.3C6720,84,6816,84,6864,84L6912,84L6912,280L6864,280C6816,280,6720,280,6624,280C6528,280,6432,280,6336,280C6240,280,6144,280,6048,280C5952,280,5856,280,5760,280C5664,280,5568,280,5472,280C5376,280,5280,280,5184,280C5088,280,4992,280,4896,280C4800,280,4704,280,4608,280C4512,280,4416,280,4320,280C4224,280,4128,280,4032,280C3936,280,3840,280,3744,280C3648,280,3552,280,3456,280C3360,280,3264,280,3168,280C3072,280,2976,280,2880,280C2784,280,2688,280,2592,280C2496,280,2400,280,2304,280C2208,280,2112,280,2016,280C1920,280,1824,280,1728,280C1632,280,1536,280,1440,280C1344,280,1248,280,1152,280C1056,280,960,280,864,280C768,280,672,280,576,280C480,280,384,280,288,280C192,280,96,280,48,280L0,280Z">
+        </path>
+    </svg>
+
+    <section>
+        <h2>Seguinos en:</h2>
         <div>
             <a href="#">
-                <span class="icon-facebook2"></span>
+                <i class="fab fa-facebook"></i>
             </a>
             <a href="#">
-                <span class="icon-instagram"></span>
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+                <i class="fab fa-whatsapp"></i>
             </a>
         </div>
     </section>
-    <section class="subcripcion">
-        <h2>Recibe novedades</h2>
-        <p>¡ingresa el mail y recibe el catalogo semanal!</p>
-        <input type="text">
-    </section>
     
-    <h4>Sitio creado por LC Desing <br>- Todos los derechos reservados -</h6>
+    <h4>Sitio creado por Lucas Conde <br>- Todos los derechos reservados -</h6>
 </footer>
