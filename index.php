26f1dcf7ccaf07c4707adf172cb45a43ba176cc2
Lucas Conde <lucasconde742@gmail.com>
2025-04-30 00:04:09 -0300

    Reestructurando todo el index para obtener algo mucho mejor

--- index.php
+++ index.php
@@ -1,33 +1,30 @@
 <?php
-    include('base/conexion.php');
+    include('conexion/conexion.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dew Rosas</title>
+    <?php
+        include('layout/meta.php');
+    ?>
 
+    <title>Dew Rosas</title>
     <link rel="stylesheet" href="css/index.css">
 
     <!-- FONTAWESOME  -->
     <script src="https://kit.fontawesome.com/439ee37b3b.js" crossorigin="anonymous"></script>
-
     <!-- BOXICONS  -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
 </head>
 <body>
-
     <?php
         include('layout/header.php');
     ?>
 
-
     <main class="contenedor-index">
         <section id="destacados">
-            <h2>
+            <h2 class="titulo">
                 <i class="fas fa-fire-alt"></i>
                 <i class="fas fa-fire-alt"></i>
                 Más vendidos
@@ -35,8 +32,8 @@
                 <i class="fas fa-fire-alt"></i>
             </h2>
             <div id="cCarousel">
-                <i class="fas fa-chevron-circle-left" id="prev" style="color:rgb(255, 255, 255);"></i>
-                <i class="fas fa-chevron-circle-right" id="next" style="color: rgb(255, 255, 255);"></i>
+                <i class="fas fa-chevron-circle-left" id="prev" style="color:rgb(0, 0, 0);"></i>
+                <i class="fas fa-chevron-circle-right" id="next" style="color: rgb(0, 0, 0);"></i>
 
                 <div id="carousel-vp">
                     <div id="cCarousel-inner">
@@ -63,16 +60,107 @@
                 </div>
             </div>
         </section>
-        
+    
         <section id="catalogo">
-            <h2>nuestro catalgoo</h2>
+            <h2 class="titulo">Nuestro Catalogo</h2>
 
-            <div>
-                <span>Conjuntos</span>
-                <span>Bodys</span>
-                <span>Bombachas</span>
-                <span>Bustier</span>
-                <span>Corsets</span>
+            <div class="warpper">
+
+                <input class="radio" id="one" name="group" type="radio" checked>
+                <input class="radio" id="two" name="group" type="radio">
+                <input class="radio" id="three" name="group" type="radio">
+
+
+                <div class="tabs">
+                    <label class="tab" id="one-tab" for="one">Conjuntos</label>
+                    <label class="tab" id="two-tab" for="two">Bombachas</label>
+                    <label class="tab" id="three-tab" for="three">Corpiños</label>
+                </div>
+
+                <div class="panels">
+                    <div class="panel" id="one-panel">
+                        <div>
+                            <?php
+                                $resultado = $conexion ->query("SELECT *
+                                                                    FROM productos WHERE id_categoria = 1 
+                                                                    LIMIT 3")or die($conexion -> error);
+                                while($fila =mysqli_fetch_array($resultado)){ ?>
+                                    <article class="cCarousel-item">
+                                        <figure>
+                                            <!-- AGREGAR IMAGEN A LA CARPETA "assets/img" Y CAMBIAR example.jpg <?php echo $fila['nombre']; ?> -->
+                                            <img src="assets/img/example.jpg" alt="<?php echo $fila['nombre']; ?>">
+                                        </figure>
+                                        <div class="infos">
+                                            <h3><?php echo $fila['nombre']; ?></h3>
+                                            <span><?php echo $fila['precio']; ?></span>
+                                            <button type="button">
+                                                <a href="enlaces/producto.php?id=<?php echo $fila['id']; ?>">
+                                                    Ver
+                                                </a> 
+                                            </button>
+                                        </div>
+                                    </article>
+                            <?php } ?>
+                        </div>
+                        <a href="#">LINKS</a>
+                    </div>
+
+                    <div class="panel" id="two-panel">
+                        <div>
+                            <?php
+                                $resultado = $conexion ->query("SELECT *
+                                                                    FROM productos WHERE id_categoria = 2 
+                                                                    LIMIT 3")or die($conexion -> error);
+                                while($fila =mysqli_fetch_array($resultado)){ ?>
+                                    <article class="cCarousel-item">
+                                        <figure>
+                                            <!-- AGREGAR IMAGEN A LA CARPETA "assets/img" Y CAMBIAR example.jpg <?php echo $fila['nombre']; ?> -->
+                                            <img src="assets/img/example.jpg" alt="<?php echo $fila['nombre']; ?>">
+                                        </figure>
+                                        <div class="infos">
+                                            <h3><?php echo $fila['nombre']; ?></h3>
+                                            <span><?php echo $fila['precio']; ?></span>
+                                            <button type="button">
+                                                <a href="enlaces/producto.php?id=<?php echo $fila['id']; ?>">
+                                                    Ver
+                                                </a> 
+                                            </button>
+                                        </div>
+                                    </article>
+                            <?php } ?>
+                        </div>
+
+                        <a href="#">LINKS</a>
+                    </div>
+
+                    <div class="panel" id="three-panel">
+                        <div>
+                            <?php
+                                $resultado = $conexion ->query("SELECT *
+                                                                    FROM productos WHERE id_categoria = 3 
+                                                                    LIMIT 3")or die($conexion -> error);
+                                while($fila =mysqli_fetch_array($resultado)){ ?>
+                                    <article class="cCarousel-item">
+                                        <figure>
+                                            <!-- AGREGAR IMAGEN A LA CARPETA "assets/img" Y CAMBIAR example.jpg <?php echo $fila['nombre']; ?> -->
+                                            <img src="assets/img/example.jpg" alt="<?php echo $fila['nombre']; ?>">
+                                        </figure>
+                                        <div class="infos">
+                                            <h3><?php echo $fila['nombre']; ?></h3>
+                                            <span><?php echo $fila['precio']; ?></span>
+                                            <button type="button">
+                                                <a href="enlaces/producto.php?id=<?php echo $fila['id']; ?>">
+                                                    Ver
+                                                </a> 
+                                            </button>
+                                        </div>
+                                    </article>
+                            <?php } ?>
+                        </div>
+
+                        <a href="#">LINKS</a>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
