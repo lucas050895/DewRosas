62684b57e569b2fa90da34e944cc444d4b9cf8de
Lucas Conde <lucasconde742@gmail.com>
2025-04-30 00:02:16 -0300

    Agregando layout meta

---
+++ layout/meta.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Este sitio en un carrito de compras de una lenceria de ropa femenina.">
+<meta name="keywords" content="website, lenceria, ropa, femenina, carrito de compras, ecommerce, tienda online">
+<meta name="robots" content="index, follow">
+<meta name="author" content="Lucas Conde">
