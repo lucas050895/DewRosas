f725911605a2730e5cee88a2ec03fdd816cc92fc
Lucas Conde <lucasconde742@gmail.com>
2025-05-01 00:31:43 -0300

    Nuevas rutas y nuevo estilo para el carrito

--- layout/header.php
+++ layout/header.php
@@ -7,7 +7,7 @@
         </div>
         <div class="menu-overlay">
             <ul class="menu-items">
-            <li><a href="#">Inicio</a></li>
+            <li><a href="http://lucasconde.ddns.net/DewRosas">Inicio</a></li>
             <li><a href="#">Categorias</a></li>
             <li><a href="#">Acerca de</a></li>
             <li><a href="#">Contacto</a></li>
@@ -16,13 +16,15 @@
     </nav>
 
     <h1>
-        <a href="http://192.168.1.95/DewRosas">
+        <a href="http://lucasconde.ddns.net/DewRosas">
             Dew Roses<br>
             <span>LENCERIA</span>
         </a>
     </h1>
 
     <div class="carrito">
-        <span class="icon-shopping-cart"></span>
+        <a href="http://lucasconde.ddns.net/DewRosas/links/carrito.php">
+            <i class='bx bx-cart'></i>
+        </a>
     </div>
 </header>
