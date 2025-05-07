e167e9f5ca4c020783975190f5ca8d7385ed9b4d
Lucas Conde <lucasconde742@gmail.com>
2025-05-07 20:57:37 -0300

    Agregando enlaces de iconos

---
+++ layout/iconos.php
@@ -0,0 +1,5 @@
+<!-- FONTAWESOME  -->
+<script src="https://kit.fontawesome.com/439ee37b3b.js" crossorigin="anonymous"></script>
+
+<!-- BOXICONS  -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
